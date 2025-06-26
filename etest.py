ad88d308a013095cb849558a425bfe2c1cfcd20d
ST-Server <simonpek58@gmail.com>
2025-06-26 22:07:10 +0800

    master

--- etest.py
+++ etest.py
@@ -315,8 +315,8 @@ def login():
                         st.session_state.examRandom = bool(getParam("考试题库每次随机生成", st.session_state.StationCN))
                     sql = f"UPDATE users set activeUser = 1, loginTime = {st.session_state.loginTime}, activeTime_session = 0, actionUser = '空闲' where userName = {st.session_state.userName}"
                     execute_sql_and_commit(conn, cur, sql)
-                    sql = "UPDATE verinfo set pyLM = pyLM + 1 where pyFile = 'visitcounter'"
-                    execute_sql_and_commit(conn, cur, sql)
+                    #sql = "UPDATE verinfo set pyLM = pyLM + 1 where pyFile = 'visitcounter'"
+                    #execute_sql_and_commit(conn, cur, sql)
                     ClearTables()
                     # transform Key to Encrypt(temporary)
                     #print(getUserEDKeys("", "enc"))
@@ -3900,7 +3900,7 @@ if st.session_state.logged_in:
         #st.markdown(f"<font size=5><center>**用户评价: {EMOJI[int(likeCM) - 1][0]} {likeCM} :orange[I feel {EMOJI[int(likeCM) - 1][1]}]**</center></font>", unsafe_allow_html=True)
         #st.markdown(f"<font size=4><center>**更新内容: {UPDATETYPE['New']}/{UPDATETYPE['Optimize']} 练习模式为每个用户增加单独的题型设置并简化操作**</center></font>", unsafe_allow_html=True)
 
-        displayAppInfo()
+        #displayAppInfo()
         displayVisitCounter()
 
     elif selected == "生成题库" or selected == "选择考试":
